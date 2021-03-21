43772e69a2e0909b248f997fa45c7256329efac2
Emir Zejnilovic <emir.zejnilovic@prointer.ba>
2021-03-21 16:16:28 +0100

    swagger addition

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
@@ -6,6 +7,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1')
   app.useGlobalPipes(new ValidateInputPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('GA Docs')
+    .setDescription('The GA API documentation')
+    .setVersion('1.0')
+    .addTag('cars')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
