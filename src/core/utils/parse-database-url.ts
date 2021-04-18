6a052d63763b88d4f3d43097d457548f4b7e990d
emchoz-ez <emir.zejnilovic@gmail.com>
2021-04-18 08:20:20 +0200

    dialect options

--- src/core/utils/parse-database-url.ts
+++ src/core/utils/parse-database-url.ts
@@ -1,12 +1,12 @@
 interface ParsedDatabaseOptions {
-    protocol: string | undefined
-    host: string | undefined
-    username: string | undefined
-    password: string | undefined
-    database: string | undefined
-    dialect: string | undefined
-    ssl?: any
-    native?: any
+  protocol: string | undefined;
+  host: string | undefined;
+  username: string | undefined;
+  password: string | undefined;
+  database: string | undefined;
+  dialect: string | undefined;
+  ssl?: any;
+  dialectOptions: any;
 }
 
 /**
@@ -22,50 +22,53 @@ interface ParsedDatabaseOptions {
  * @see https://stackoverflow.com/questions/45073320/regex-for-a-url-connection-string
  */
 const parseDatabaseUrl = (
-    databaseUrl: string | undefined
+  databaseUrl: string | undefined,
 ): ParsedDatabaseOptions => {
-    if (!databaseUrl)
-        return {
-            protocol: undefined,
-            username: undefined,
-            host: undefined,
-            password: undefined,
-            database: undefined,
-            dialect: 'postgres',
-            ssl: true,
-            native: true,
-        }
-
-    const regex: RegExp = /^(?:([^:\/?#\s]+):\/{2})?(?:([^@\/?#\s]+)@)?([^\/?#\s]+)?(?:\/([^?#\s]*))?(?:[?]([^#\s]+))?\S*$/
-
-    const matches: RegExpMatchArray | null = databaseUrl.match(regex)
-
-    if (!matches)
-        return {
-            protocol: undefined,
-            username: undefined,
-            host: undefined,
-            password: undefined,
-            database: undefined,
-            dialect: 'postgres',
-            ssl: true,
-            native: true,
-        }
-
+  if (!databaseUrl)
     return {
-        protocol: matches[1] || undefined,
-        username:
-            matches[2] !== undefined ? matches[2].split(':')[0] : undefined,
-        host:
-            matches[3] != undefined
-                ? matches[3].split(/:(?=\d+$)/)[0]
-                : undefined,
-        database:
-            matches[4] != undefined ? matches[4].split('/')[0] : undefined,
-        password: matches[2] != undefined ? matches[2].split(':')[1] : undefined,
-        dialect: 'postgres',
-        ssl: true,  native: true,
-    }
-}
+      protocol: undefined,
+      username: undefined,
+      host: undefined,
+      password: undefined,
+      database: undefined,
+      dialect: 'postgres',
+      ssl: true,
+      dialectOptions: {
+        ssl: true,
+      },
+    };
 
-export default parseDatabaseUrl
+  const regex: RegExp = /^(?:([^:\/?#\s]+):\/{2})?(?:([^@\/?#\s]+)@)?([^\/?#\s]+)?(?:\/([^?#\s]*))?(?:[?]([^#\s]+))?\S*$/;
+
+  const matches: RegExpMatchArray | null = databaseUrl.match(regex);
+
+  if (!matches)
+    return {
+      protocol: undefined,
+      username: undefined,
+      host: undefined,
+      password: undefined,
+      database: undefined,
+      dialect: 'postgres',
+      ssl: true,
+      dialectOptions: {
+        ssl: true,
+      },
+    };
+
+  return {
+    protocol: matches[1] || undefined,
+    username: matches[2] !== undefined ? matches[2].split(':')[0] : undefined,
+    host:
+      matches[3] != undefined ? matches[3].split(/:(?=\d+$)/)[0] : undefined,
+    database: matches[4] != undefined ? matches[4].split('/')[0] : undefined,
+    password: matches[2] != undefined ? matches[2].split(':')[1] : undefined,
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: true,
+    },
+  };
+};
+
+export default parseDatabaseUrl;
