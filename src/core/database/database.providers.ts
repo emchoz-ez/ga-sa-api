8e1a375687d3fa596f47a0108fe4f4a26aa06cbf
emchoz-ez <emir.zejnilovic@gmail.com>
2021-04-18 13:03:12 +0200

    novi pokusaj

--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -4,30 +4,41 @@ import { CarType } from 'src/modules/codebooks/car-type/entities/car-type.entity
 import { User } from 'src/modules/users/user.entity';
 import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
 import { databaseConfig } from './database.config';
-import parseDatabaseUrl from '../utils/parse-database-url'
+import parseDatabaseUrl from '../utils/parse-database-url';
 
-export const databaseProviders = [{
+export const databaseProviders = [
+  {
     provide: SEQUELIZE,
     useFactory: async () => {
-        let config;
-        const parsedConfig = parseDatabaseUrl(databaseConfig.production.uri);
-        console.log(parsedConfig, 'KATOLICI');
-        switch (process.env.NODE_ENV) {
+      let config;
+      const parsedConfig = parseDatabaseUrl(databaseConfig.production.uri);
+      console.log(parsedConfig, 'KATOLICI');
+      switch (process.env.NODE_ENV) {
         case DEVELOPMENT:
-           config = databaseConfig.development;
-           break;
+          config = databaseConfig.development;
+          break;
         case TEST:
-           config = databaseConfig.test;
-           break;
+          config = databaseConfig.test;
+          break;
         case PRODUCTION:
-           config = parsedConfig;
-           break;
+          config = parsedConfig;
+          break;
         default:
-           config = databaseConfig.development;
-        }
-        const sequelize = new Sequelize(config);
-        sequelize.addModels([User, Car, CarType]);
-        await sequelize.sync();
-        return sequelize;
+          config = databaseConfig.development;
+      }
+      let sequelize: any;
+      process.env.NODE_ENV === 'PRODUCTION'
+        ? (sequelize = new Sequelize(config.connectionString, {
+            dialect: 'postgres',
+            protocol: 'postgres',
+            port: 5432,
+            host: config.host,
+            logging: true,
+          }))
+        : (sequelize = new Sequelize(config));
+      sequelize.addModels([User, Car, CarType]);
+      await sequelize.sync();
+      return sequelize;
     },
-}];
+  },
+];
